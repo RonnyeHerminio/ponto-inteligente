792dd93595c9ce6c6897c9c10de8a20c1afe9c0b
Ronnye Herminio <rpherminio@hotmail.com>
2018-02-15 13:41:04 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/example/pontointeligente/api/PontoInteligente2Application.java
@@ -0,0 +1,12 @@
+package com.example.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligente2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligente2Application.class, args);
+	}
+}
